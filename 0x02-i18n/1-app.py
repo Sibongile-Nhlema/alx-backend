9ed784364ea7b3b5a4808d82969eef4b854258aa
Sibongile <sibongile.nhlema@gmail.com>
2024-07-02 18:31:46 +0000

    created the basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+Module for the basic babel setup
+'''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    '''
+    Class that handles configuration of availiable languages
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    '''
+    Method handles the rendering of the index template
+    '''
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
