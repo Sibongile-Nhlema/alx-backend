4fbf64e1c19843451104960031fa0125277f213b
Sibongile <sibongile.nhlema@gmail.com>
2024-07-02 18:00:11 +0000

    created the single path and index.html file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,7 +2,16 @@
 '''
 The module for the Flask Appication
 '''
-from flask import app
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return render_template('0-index/html')
+
+
+if __name__ = '__main__':
+    app.run(debug=True)
