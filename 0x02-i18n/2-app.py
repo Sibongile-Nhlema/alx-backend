b9b1e3707e3ff5c29e7c2680b3caa46e05230072
Sibongile <sibongile.nhlema@gmail.com>
2024-07-02 18:45:00 +0000

    implemented the get_locale function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,7 @@
 '''
 Module for the basic babel setup
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -20,12 +20,21 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    '''
+    Selects the best match for supported languages based on
+    the client's request.
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     '''
     Method handles the rendering of the index template
     '''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
