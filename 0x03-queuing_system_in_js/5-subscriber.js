c81b6d9b0711325abd9ae076c0cf2d26a932b736
Sibongile <sibongile.nhlema@gmail.com>
2024-07-30 21:01:07 +0200

    implemented Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+
+// Create a Redis client
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Subscribe to the channel 'holberton school channel'
+client.subscribe('holberton school channel');
+
+// Handle incoming messages
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
